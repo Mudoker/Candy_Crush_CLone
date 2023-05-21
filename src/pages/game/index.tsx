c1a19df08f88ef87609d9abde0faf9413bddf342
manuel-hans <45839264+manuel-hans@users.noreply.github.com>
2023-05-21 13:10:12 +0700

    Show target info in stage selection

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -4,10 +4,10 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import React, {useEffect, useRef, useState} from 'react'
 import { convertInitialBoard } from "@/components/candy-crush/game";
-import { fetchAvailableStages } from "./available-stages";
+import { availableStageType, fetchAvailableStages } from "./available-stages";
 
 export default function GamePage(){
-  const [availableStages, setAvailableStages] = useState<boardType[]>([])
+  const [availableStages, setAvailableStages] = useState<availableStageType[]>([])
   const [selectedIndex, setSelectedIndex] = useState(0)
   const [boardSize, setBoardSize] = useState(0)
 
@@ -23,7 +23,7 @@ export default function GamePage(){
     }
     async function init(){
       const result = await fetchAvailableStages()
-      const processedStages = result.availableStages.map(({board}) => convertInitialBoard(board))
+      const processedStages = result.availableStages
       setAvailableStages(processedStages)
       calculateTileSize()
       window.addEventListener("resize", calculateTileSize)
@@ -34,13 +34,28 @@ export default function GamePage(){
     <main>
       <div className={`flex bg-orange-300 p-4`} style={{minHeight: "100svh"}}>
         <div className={`m-auto flex flex-col border-4 border-orange-600 bg-orange-200 rounded-lg w-full max-w-md p-4`}>
-          <img src={`/logo.svg`} />
+          <img src={`/logo.svg`} className="w-3/4 mx-auto"/>
           <div ref={tilesContainer} className="mb-4">
             <Carousel onChange={setSelectedIndex} showIndicators={false} showThumbs={false} showStatus={false}>
               {
                 availableStages.map((availableStage, i) => (
-                  <div className={`flex items-center justify-center`}>
-                    <Board key={i} board={availableStage} boardSize={boardSize}></Board>
+                  <div className={`flex flex-col items-center justify-center`} key={i}>
+                    <div className={`bg-orange-50 border-orange-600 border-4 rounded-lg text-orange-600 mb-2`}>
+                      <p className="text-xl font-bold mb-2">Target</p>
+                      <div className="flex flex-wrap items-center justify-center">
+                        {
+                          Object.keys(availableStage.goals).map((piece, j) => {
+                            return (
+                              <div key={j} className="flex items-center justify-center mx-2">
+                                <img src={`/pieces/piece-${piece}.svg`} className="w-8 h-8 mr-2" />
+                                <p className="whitespace-nowrap">X {availableStage.goals[piece]}</p>
+                              </div>
+                            )
+                          })
+                        }
+                      </div>
+                    </div>
+                    <Board board={convertInitialBoard(availableStage.board)} boardSize={boardSize}></Board>
                   </div>
                 ))
               }
