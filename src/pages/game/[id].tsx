069851dc8fb9d59149e5e6335970e5339051ca74
manuel-hans <45839264+manuel-hans@users.noreply.github.com>
2023-05-21 14:42:46 +0700

    Create [id].tsx

---
+++ src/pages/game/[id].tsx
@@ -0,0 +1,30 @@
+import Game from "@/components/candy-crush/game";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { availableStageType, fetchStage } from "./available-stages";
+
+export default function Page(){
+  const router = useRouter()
+  const [stage, setStage] = useState<availableStageType>()
+  const id = router.query
+  useEffect(() => {
+    async function init(){
+      const response = await fetchStage(router.query.id)
+      if(!response?.success){
+        return
+      }
+      setStage(response.stage)
+    }
+    init()
+  }, [id])
+  return (
+    <main>
+      <div className={`flex bg-orange-300 p-4`} style={{minHeight: "100svh"}}>
+        {
+          !!stage ? <Game availablePieces={stage.availablePieces} initialBoard={stage.board} moveCount={stage.moveCount} goals={stage.goals}></Game>
+          : null
+        }
+      </div>
+    </main>
+  )
+}
