e1dafd1210bed51ee44e7294260ce2cc95f5084f
manuel-hans <45839264+manuel-hans@users.noreply.github.com>
2023-06-06 19:33:56 +0700

    Remove unused files

--- src/class/CandyCrushType.d.ts
+++
@@ -1,6 +0,0 @@
-import CandyCrushPiece from "./CandyCrushPiece"
-import CandyCrushTile from "./CandyCrushTile"
-
-type CandyCrushRow = Array<CandyCrushTile>
-type CandyCrushBoard = Array<CandyCrushRow>
-type CandyCrushCoordinate = {x: number, y: number}
